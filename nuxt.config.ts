c3e312e6e636d0a22b04a21eeb844f8e66116650
cblokkeel <cblokkeeltech@gmail.com>
2025-05-14 18:55:51 +0200

    chore: add posthog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/icon'],
-  routeRules: {
-    '/': { prerender: true }
-  },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-})
+	devtools: { enabled: true },
+	modules: ["@nuxt/content", "@nuxt/icon"],
+	routeRules: {
+		"/": { prerender: true },
+	},
+	css: ["~/assets/css/main.css"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	runtimeConfig: {
+		public: {
+			posthogPublicKey: "phc_HJ506FuJybkDaYSl76MCbcct9t7yUjT2u7qqgZaJZOK",
+			posthogHost: "https://eu.i.posthog.com",
+		},
+	},
+});
+
